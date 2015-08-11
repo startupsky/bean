03ad5a05e2af25745e443ae72b633a85d4567a50
fresky <dawei.xu@gmail.com>
2015-08-11 19:01:39 +0800

    support log in and sign in

--- src/game/player_mgr.go
+++ src/game/player_mgr.go
@@ -5,18 +5,20 @@ import (
 	"net"
 	"strconv"
 	"util"
+	"user"
 )
 
 type PlayerManager struct {
 	parent        *Container
 	onlinePlayers util.BeanSlice
+	userManager *user.UserManager
 }
 
 func NewPlayerManager(parent *Container) *PlayerManager {
 	this := new(PlayerManager)
 	this.onlinePlayers = util.BeanSlice{}
 	this.parent = parent
-
+	this.userManager = user.NewUserManager()
 	return this
 }
 
@@ -24,17 +26,30 @@ func (this *PlayerManager) Login(conn net.Conn) (player *Player, err error) {
 	proto := this.parent.parent.proto
 	if cmd, err := proto.ReadCommand(bufio.NewReader(conn)); err == nil {
 		if cmd.CommandID == CMDLOGIN && len(cmd.Arguments) == 2 {
+			userid := cmd.Arguments[0]
 			id, _ := strconv.ParseUint(cmd.Arguments[0], 10, 64)
 			passwd := cmd.Arguments[1]
-			//valid logic here
 
 			resp := proto.CreateResponse()
-			resp.ErrNo = ErrOK
-			resp.Data = []string{"1"}
-			if _, err := conn.Write(resp.Serialize()); err == nil {
-				player = NewPlayer(id, passwd, "dislay name", conn, this)
-				this.onlinePlayers = append(this.onlinePlayers, player)
-				return player, nil
+			
+			user := this.userManager.AddUser(userid, passwd)
+			if user == nil {
+				user = this.userManager.GetUser(userid, passwd)
+			}else{
+				this.userManager.Save()
+			}
+			
+			if user == nil{
+				resp.ErrNo = ErrFormat
+				resp.Data = []string{"0"}
+			}else{
+				resp.ErrNo = ErrOK
+				resp.Data = []string{"1"}
+				if _, err := conn.Write(resp.Serialize()); err == nil {
+					player = NewPlayer(id, passwd, "dislay name", conn, this)
+					this.onlinePlayers = append(this.onlinePlayers, player)
+					return player, nil
+				}
 			}
 		}
 	}
